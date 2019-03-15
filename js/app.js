6dba515ffbf1ab6dfa0b0109d8aa96aacdb9b2ee
Gennadiy <ggm_kharkiv@ukr.net>
2019-03-15 11:30:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 const title = document.querySelector("#title");
+const platform = navigator.platform;
+const pdf_url = "../prekurs_ssylki_na_stati.pdf";
 title.textContent = navigator.platform;
+
+if (platform.includes("aarch64")) {
+  Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(pdf_url));
+  startActivity(browserIntent);
+}
